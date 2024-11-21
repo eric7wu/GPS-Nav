31938760e71d4369e88670063fc1875d65143de3
Eric Wu <ericwu156@gmail.com>
2024-11-20 20:14:38 -0500

    Added remove waypoint function

--- GPSCommands/commands.cpp
+++ GPSCommands/commands.cpp
@@ -11,7 +11,7 @@ string retrieve(vector<string> coordinates, int pos)
 vector<string> add_waypoint(vector<string> coordinates, string new_point, int pos)
 {
     vector<string> updated_coordinates;
-    for (int n = 0; n <= coordinates.size(); n++)
+    for (int n = 0; n < coordinates.size(); n++)
     {
         if (n + 1 == pos)
         {
@@ -25,12 +25,36 @@ vector<string> add_waypoint(vector<string> coordinates, string new_point, int po
     return updated_coordinates;
 }
 
+vector<string> remove_waypoint(vector<string> coordinates, int pos)
+{
+    vector<string> updated_coordinates;
+    for (int n = 0; n < coordinates.size(); n++)
+    {
+        if (n + 1 != pos)
+        {
+            updated_coordinates.push_back(coordinates.at(n));
+        }
+    }
+    return updated_coordinates;
+}
+
 /*
 int main()
 {
     vector<string> coordinates = {"43.470746, -80.553317", "43.472182, -80.547994"};
     cout << "Retrieved coordinate: " << retrieve(coordinates, 2) << endl;
     vector<string> new_coordinates = add_waypoint(coordinates, "43.400746, -80.603317", 1);
+    cout << "Updated coordinates: ";
+    for (int i = 0; i < new_coordinates.size(); i++)
+    {
+        cout << new_coordinates[i];
+        if (i != new_coordinates.size() - 1)
+        {
+            cout << " | ";
+        }
+    }
+    cout << endl;
+    new_coordinates = remove_waypoint(new_coordinates, 2);
     cout << "Updated coordinates: ";
     for (int i = 0; i < new_coordinates.size(); i++)
     {
